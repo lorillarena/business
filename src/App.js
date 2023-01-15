903dafd50195b19325f8bf77b6319ba9f9f6f111
Lori Llarena <lorillarena@gmail.com>
2023-01-14 23:12:00 -0500

    Tailwind added

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from './components/views/Header';
+import './App.css';
+
+
+function App() {
+	return (
+		<>
+			<Header />
+			<main></main>
+			<footer></footer>
+		</>
+	);
+}
+
+export default App;
